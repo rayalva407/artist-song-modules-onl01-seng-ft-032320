8f8bb60cd210e61c188eef2a21cc8d0033cbbd16
Ray Alva <rayalva407@gmail.com>
2020-04-09 21:15:00 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,7 +3,7 @@ module Findable
   module ClassMethods
     
     def find_by_name
-      def self.find_by_name(name)
+      def find_by_name(name)
       self.all.detect{|a| a.name == name}
     end
   end

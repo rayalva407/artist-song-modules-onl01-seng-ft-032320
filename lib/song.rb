ca43c92e8f683b27b9e3655aa3725d766e59ff80
Ray Alva <rayalva407@gmail.com>
2020-04-09 21:40:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
-  inclued Memorable::InstanceMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist

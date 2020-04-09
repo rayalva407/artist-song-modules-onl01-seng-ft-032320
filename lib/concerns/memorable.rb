1525e06c345ebca1e03e994791f3c2cfdebbd4fc
Ray Alva <rayalva407@gmail.com>
2020-04-09 20:37:36 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,10 @@
 module Memorable
   module ClassMethods
-  def self.reset_all
-   self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+    def self.reset_all
+     self.all.clear
+    end
+  
+    def self.count
+      self.all.count
+    end
 end
